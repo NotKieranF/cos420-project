1efc4291e5f0d29e3b1a21b8189bc139e30e3cdf
Kieran Firkin <89042089+NotKieranF@users.noreply.github.com>
2024-03-02 16:10:50 -0500

    Added jumping + cleanup
    - Added a variable height jump where upwards velocity is halved when jump is released early.
    - Groundedness is based off of a collider check beneath the players feet, it may be a good idea to modify this in the future to check if a collider is of the right type.
    - Cleaned up the PlayerMovement script a bit.

--- Double Dash Dodge & Deceive/Assets/Scripts/PlayerMovement.cs
+++ Double Dash Dodge & Deceive/Assets/Scripts/PlayerMovement.cs
@@ -6,49 +6,78 @@ using UnityEngine.InputSystem; // This is the input system package for WASD move
 
 public class PlayerMovement : MonoBehaviour
 {
-// Making the player sprite be affected by gravity
-    public Rigidbody2D rigidBody;
+    // Internal component references
+    private Rigidbody2D m_RigidBody;
+    private Collider2D m_Collider;
+    private Animator m_Animator;
+    
+    // Player movement parameters
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpVelocity = 10f;
+    [SerializeField] private float jumpCancelFactor = 0.5f;
 
-// Animator class initialization
-    Animator animator;
-    // Player speed is 5 units per second
-    public float speed = 5f;
+    // Stores input for horizontal movement
+    private float xAxisMovement;
 
-// Stores input for horizontal movement
-    float xAxisMovement;
-
-    bool isFacingRight = false;
+    private bool isFacingRight = false;
+    private bool isGrounded = false;
+    private bool isJumping = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        rigidBody = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
-        
+        // Get references to internal components
+        m_RigidBody = GetComponent<Rigidbody2D>();
+        m_Animator = GetComponent<Animator>();
+        m_Collider = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-    // Updates the velocity of the player and maintains the vertical velocity
-        rigidBody.velocity = new Vector2 (xAxisMovement * speed, rigidBody.velocity.y);
+        // Updates the velocity of the player and maintains the vertical velocity
+        m_RigidBody.velocity = new Vector2 (xAxisMovement * speed, m_RigidBody.velocity.y);
 
+        // Updates the animator component with the player's velocity
+        m_Animator.SetFloat("xVelocity", Math.Abs(m_RigidBody.velocity.x));
         FlipAnimation();
         
-    // Updates the animator component with the player's velocity
-        animator.SetFloat("xVelocity", Math.Abs(rigidBody.velocity.x));
+        // Check for groundedness
+        CheckGround();
     }
 
     // This method is called by an input action like WASD or arrow keys 
     public void Move(InputAction.CallbackContext context)
     {
-    // Vector2 describes the X and Y position of a game object
+        // Vector2 describes the X and Y position of a game object
         xAxisMovement = context.ReadValue<Vector2>().x;
+    }
+    
+    public void Jump(InputAction.CallbackContext context)
+    {
+        isJumping = context.ReadValue<float>() > 0;
+        if (isJumping && isGrounded)
+        {
+            m_RigidBody.velocity += Vector2.up * jumpVelocity;
+        }
+        else if (m_RigidBody.velocity.y > 0)
+        {
+            // Reduce upwards velocity if jump is released early
+            m_RigidBody.velocity *= new Vector2(1.0f, jumpCancelFactor);
+        }
+    }
+
+    void CheckGround()
+    {
+        Vector2 min = new Vector2(m_Collider.bounds.min.x + 0.05f, m_Collider.bounds.min.y - 0.05f);
+        Vector2 max = new Vector2(m_Collider.bounds.max.x - 0.05f, m_Collider.bounds.min.y - 0.05f);
+        isGrounded = Physics2D.OverlapArea(min, max) != null;
+        //Debug.DrawLine(min, max, Color.red);
     }
 
     void FlipAnimation()
     {
-    // If the character is moving right, flip them to the right, and vice versa
+        // If the character is moving right, flip them to the right, and vice versa
         if (isFacingRight && xAxisMovement > 0f || !isFacingRight && xAxisMovement < 0f)
         {
             isFacingRight = !isFacingRight;
@@ -60,6 +89,5 @@ public class PlayerMovement : MonoBehaviour
             transform.localScale = ls;
 
         }
-
     }
 }
