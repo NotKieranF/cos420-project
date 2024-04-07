bc864f18395e65829042a7017034a688b59160ae
Mariia Maksymenko <mariia.maksymenko18@gmail.com>
2024-04-07 18:19:32 -0400

    Added Wall Jumping and Sliding
    Wall sliding animation for both will need to be added and jumping for player 1

--- Double Dash Dodge & Deceive/Assets/Scripts/PlayerMovement.cs
+++ Double Dash Dodge & Deceive/Assets/Scripts/PlayerMovement.cs
@@ -19,6 +19,10 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float speed = 5f;
     [SerializeField] private float jumpVelocity = 10f;
     [SerializeField] private float jumpCancelFactor = 0.5f;
+    [SerializeField] private float wallSlideSpeed = 3f;
+    [SerializeField] private float wallJumpVelocity = 8f;
+    
+    [SerializeField] private LayerMask wallLayer;
 
     // Stores input for horizontal movement
     private float xAxisMovement;
@@ -26,8 +30,11 @@ public class PlayerMovement : MonoBehaviour
     private bool isFacingRight = false;
     private bool isGrounded = false;
     private bool isJumping = false;
-
+    private bool isTouchingWall = false;
+    private bool isWallSliding = false;
     private bool menuPressed = true;
+    private Vector2 wallNormal;
+
 
     // Start is called before the first frame update
     void Start()
@@ -41,16 +48,22 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Check for groundedness
+        CheckGround();
+
+        // Check if touching wall
+        CheckWall();
+
         // Updates the velocity of the player and maintains the vertical velocity
         m_RigidBody.velocity = new Vector2 (xAxisMovement * speed, m_RigidBody.velocity.y);
-
+        
         // Updates the animator component with the player's velocity
         m_Animator.SetFloat("xVelocity", Math.Abs(m_RigidBody.velocity.x));
         m_Animator.SetFloat("yVelocity", m_RigidBody.velocity.y);
+
         FlipAnimation();
-        
-        // Check for groundedness
-        CheckGround();
+
+        WallSlide();
         
     }
 
@@ -71,7 +84,10 @@ public class PlayerMovement : MonoBehaviour
                 isJumping = true;
                 m_RigidBody.velocity += Vector2.up * jumpVelocity;
                 m_Animator.SetBool("isJumping", isJumping);
-                //FlipAnimation();
+            }
+            else if (isWallSliding)
+            {
+                WallJump();
             }
         }
         else if (context.canceled)
@@ -84,20 +100,38 @@ public class PlayerMovement : MonoBehaviour
             isJumping = false;
             m_Animator.SetBool("isJumping", isJumping);
         }
-        /*isJumping = context.ReadValue<float>() > 0;
-        if (isJumping && isGrounded)
+    }
+
+    void WallJump()
+    {
+        Vector2 jumpDirection = wallNormal + Vector2.up;
+        isJumping = true;
+        m_RigidBody.velocity = new Vector2(jumpDirection.x * wallJumpVelocity, jumpDirection.y * jumpVelocity);
+        m_Animator.SetBool("isJumping", isJumping);
+    }
+    void CheckWall()
+    {
+        isTouchingWall = Physics2D.Raycast(transform.position, Vector2.right * transform.localScale.x, m_Collider.bounds.extents.x + 0.1f, wallLayer)
+                     || Physics2D.Raycast(transform.position, Vector2.left * transform.localScale.x, m_Collider.bounds.extents.x + 0.1f, wallLayer);
+        if (isTouchingWall)
         {
-            m_RigidBody.velocity += Vector2.up * jumpVelocity;
-            // Animator transition conditions
-            m_Animator.SetBool("isJumping", isJumping); 
-            FlipAnimation();
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right * transform.localScale.x, m_Collider.bounds.extents.x + 0.1f, wallLayer);
+            if (hit)
+                wallNormal = hit.normal;
         }
-        else if (m_RigidBody.velocity.y > 0)
+    }
+    void WallSlide()
+    {
+        if (isTouchingWall && !isGrounded && m_RigidBody.velocity.y < 0)
         {
-            // Reduce upwards velocity if jump is released early
-            m_RigidBody.velocity *= new Vector2(1.0f, jumpCancelFactor);
+            isWallSliding = true;
+            m_RigidBody.velocity = new Vector2(m_RigidBody.velocity.x, -wallSlideSpeed);
         }
-        */
+        else
+        {
+            isWallSliding = false;
+        }
+        //m_Animator.SetBool("isWallSliding", isWallSliding); - Uncomment when wall sliding animation exists
     }
 
     void CheckGround()
